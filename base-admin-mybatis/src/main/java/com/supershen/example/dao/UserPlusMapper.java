cb27995d264d6cc4e8576639db207adc8dd6864d
supershen1988 <6692315@qq.com>
2018-06-04 15:53:12 +0800

    增加layuiadmin并删除无用文件

--- base-admin-mybatis/src/main/java/com/supershen/example/dao/UserPlusMapper.java
+++
@@ -1,8 +0,0 @@
-package com.supershen.example.dao;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.supershen.example.entity.User;
-
-public interface UserPlusMapper extends BaseMapper<User> {
-
-}
